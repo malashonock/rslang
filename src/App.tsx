e754b5ba3ab61f585fa05e4cebfb7c4a914c1a74
Yan Malashonak <malashonock@gmail.com>
2022-08-16 23:00:55 +0300

    init: setup eslint & prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,5 @@
 import styles from './App.module.scss';
 
-const App = (): JSX.Element => {
-  return (
-    <div className={styles.appRoot}>
-      RS Lang App
-    </div>
-  );
+export default function App(): JSX.Element {
+  return <div className={styles.appRoot}>RS Lang App</div>;
 }
-
-export default App;
