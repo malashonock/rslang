3be2370b55abf0c64d987b3dcf595a8a18520b80
Stanlys <stanlys@yandex.ru>
2022-08-18 22:53:24 +0300

    feat: add btn register&forgot

--- src/features/auth/login-form/LoginForm.tsx
+++ src/features/auth/login-form/LoginForm.tsx
@@ -1,99 +1,3 @@
-// import { CssVarsProvider } from '@mui/joy/styles';
-// import { Button } from '@mui/joy';
-// import Box from '@mui/joy/Box';
-// // import IconButton from '@mui/joy/IconButton';
-// import InputAdornment from '@mui/material/InputAdornment';
-// import FormHelperText from '@mui/material/FormHelperText';
-// import FormControl from '@mui/material/FormControl';
-// import TextField from '@mui/material/TextField';
-// import OutlinedInput from '@mui/material/OutlinedInput';
-// import React from 'react';
-// // import styles from './LoginForm.module.scss';
-
-// interface State {
-//   amount: string;
-//   password: string;
-//   weight: string;
-//   weightRange: string;
-//   showPassword: boolean;
-// }
-
-// export default function LoginForm(): JSX.Element {
-//   const [values, setValues] = React.useState<State>({
-//     amount: '',
-//     password: '',
-//     weight: '',
-//     weightRange: '',
-//     showPassword: false,
-//   });
-
-//   const handleChange = (prop: keyof State) => (event: React.ChangeEvent<HTMLInputElement>) => {
-//     setValues({ ...values, [prop]: event.target.value });
-//   };
-
-//   // const handleClickShowPassword = () => {
-//   //   setValues({
-//   //     ...values,
-//   //     showPassword: !values.showPassword,
-//   //   });
-//   // };
-
-//   // const handleMouseDownPassword = (event: React.MouseEvent<HTMLButtonElement>) => {
-//   //   event.preventDefault();
-//   // };
-
-//   return (
-//     <CssVarsProvider>
-//       <Box component="form" sx={{ display: 'flex', flexWrap: 'wrap' }}>
-//         <TextField
-//           label="With normal TextField"
-//           id="outlined-start-adornment"
-//           sx={{ m: 1, width: '25ch' }}
-//           InputProps={{
-//             startAdornment: <InputAdornment position="start">kg</InputAdornment>,
-//           }}
-//         />
-//         <FormControl sx={{ m: 1, width: '25ch' }} variant="outlined">
-//           <OutlinedInput
-//             id="outlined-adornment-weight"
-//             value={values.weight}
-//             onChange={handleChange('weight')}
-//             endAdornment={<InputAdornment position="end">kg</InputAdornment>}
-//             aria-describedby="outlined-weight-helper-text"
-//             inputProps={{
-//               'aria-label': 'weight',
-//             }}
-//           />
-//           <FormHelperText id="outlined-weight-helper-text">Weight</FormHelperText>
-//         </FormControl>
-//         <TextField
-//           label="With normal TextField"
-//           id="outlined-start-adornment"
-//           sx={{ m: 1, width: '25ch' }}
-//           InputProps={{
-//             startAdornment: <InputAdornment position="start">kg</InputAdornment>,
-//           }}
-//         />
-//         <FormControl sx={{ m: 1, width: '25ch' }} variant="outlined">
-//           <OutlinedInput
-//             id="outlined-adornment-weight"
-//             value={values.weight}
-//             onChange={handleChange('weight')}
-//             endAdornment={<InputAdornment position="end">kg</InputAdornment>}
-//             aria-describedby="outlined-weight-helper-text"
-//             inputProps={{
-//               'aria-label': 'weight',
-//             }}
-//           />
-//           <FormHelperText id="outlined-weight-helper-text">Weight</FormHelperText>
-//         </FormControl>
-//         <TextField name="email" type="email" placeholder="johndoe@email.com" label="Email" />
-//         <TextField name="password" type="password" placeholder="password" label="Password" />
-//         <Button>Joy UI</Button>
-//       </Box>
-//     </CssVarsProvider>
-//   );
-// }
 import * as React from 'react';
 import Box from '@mui/material/Box';
 import IconButton from '@mui/material/IconButton';
@@ -102,28 +6,24 @@ import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
 import InputLabel from '@mui/material/InputLabel';
 import InputAdornment from '@mui/material/InputAdornment';
-import FormHelperText from '@mui/material/FormHelperText';
 import FormControl from '@mui/material/FormControl';
 import TextField from '@mui/material/TextField';
 import Visibility from '@mui/icons-material/Visibility';
 import VisibilityOff from '@mui/icons-material/VisibilityOff';
 import PersonIcon from '@mui/icons-material/Person';
+import Divider from '@mui/material/Divider';
 import styles from './LoginForm.module.scss';
 
 interface State {
   amount: string;
   password: string;
-  weight: string;
-  weightRange: string;
   showPassword: boolean;
 }
 
-export default function InputAdornments() {
+export default function LoginForm() {
   const [values, setValues] = React.useState<State>({
     amount: '',
     password: '',
-    weight: '',
-    weightRange: '',
     showPassword: false,
   });
 
@@ -145,11 +45,14 @@ export default function InputAdornments() {
   return (
     <Box>
       <div className={styles.form}>
+        <h3>WELCOME</h3>
         <TextField
           label="E-mail"
-          placeholder="E-mail"
-          id="standard-start-adornment"
-          sx={{ m: 1, width: '25ch' }}
+          id="user-email"
+          sx={{ m: 2, width: '40ch' }}
+          type="email"
+          fullWidth
+          autoFocus
           InputProps={{
             startAdornment: (
               <InputAdornment position="start">
@@ -159,27 +62,10 @@ export default function InputAdornments() {
           }}
           variant="standard"
         />
-        <FormControl variant="standard" sx={{ m: 1, mt: 3, width: '25ch' }}>
+        <FormControl sx={{ m: 2, width: '40ch' }} variant="standard">
+          <InputLabel htmlFor="user-password">Password</InputLabel>
           <Input
-            id="standard-adornment-weight"
-            value={values.weight}
-            onChange={handleChange('weight')}
-            endAdornment={
-              <InputAdornment position="end">
-                <PersonIcon />
-              </InputAdornment>
-            }
-            aria-describedby="standard-weight-helper-text"
-            inputProps={{
-              'aria-label': 'weight',
-            }}
-          />
-          <FormHelperText id="standard-weight-helper-text">Weight</FormHelperText>
-        </FormControl>
-        <FormControl sx={{ m: 1, width: '25ch' }} variant="standard">
-          <InputLabel htmlFor="standard-adornment-password">Password</InputLabel>
-          <Input
-            id="standard-adornment-password"
+            id="user-password"
             type={values.showPassword ? 'text' : 'password'}
             value={values.password}
             onChange={handleChange('password')}
@@ -200,9 +86,10 @@ export default function InputAdornments() {
           <Button variant="contained" color="success">
             Login
           </Button>
-          <Button variant="outlined" color="primary">
-            Exit
-          </Button>
+        </Stack>
+        <Stack direction="row" spacing={2} divider={<Divider orientation="vertical" flexItem />}>
+          <Button href="#text-buttons">Forgot Password</Button>
+          <Button href="#text-buttons">Register</Button>
         </Stack>
       </div>
     </Box>
