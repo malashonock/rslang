5b5f5d8a6b1e27b12d64c33067c172a088ba3f7b
Stanlys <stanlys@yandex.ru>
2022-08-19 14:46:35 +0300

    feat: add basic structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { Container } from 'react-bootstrap';
 import styles from './App.module.scss';
+import Footer from './features/main-page/footer/Footer';
 
 export default function App(): JSX.Element {
   return (
     <div className={styles.appRoot}>
       <Container>
         <h1>Main page content</h1>
+        <Footer />
       </Container>
     </div>
   );
--- src/features/main-page/footer/Footer.tsx
+++ src/features/main-page/footer/Footer.tsx
@@ -1,29 +1,48 @@
-import { Container, Nav, Navbar, NavDropdown, Offcanvas } from 'react-bootstrap';
+import { Container, Navbar, Image, Nav } from 'react-bootstrap';
+// import CardHeader from 'react-bootstrap/esm/CardHeader';
 
 function Footer(): JSX.Element {
   return (
-    <Navbar bg="primary" variant="dark" expand="sm">
-      <Container fluid>
-        <Navbar.Toggle aria-controls="navbar-main" />
-        <Navbar.Brand href="/">RS Lang</Navbar.Brand>
-        <Navbar.Offcanvas id="navbar-main">
-          <Offcanvas.Header closeButton>
-            <Offcanvas.Title>RS Lang</Offcanvas.Title>
-          </Offcanvas.Header>
-          <Offcanvas.Body>
-            <Nav className="me-auto">
-              <Nav.Link href="/dictionary">Dictionary</Nav.Link>
-              <NavDropdown title="Mini-games" id="nav-dropdown-games">
-                <NavDropdown.Item href="/games/audio-challenge">Audio Challenge</NavDropdown.Item>
-                <NavDropdown.Item href="/games/sprint">Sprint</NavDropdown.Item>
-              </NavDropdown>
-              <Nav.Link href="/statistics">Statistics</Nav.Link>
-            </Nav>
-          </Offcanvas.Body>
-        </Navbar.Offcanvas>
+    <Navbar fixed="bottom">
+      <Container>
+        <Nav>
+          <Navbar.Brand href="#home">RSLang 2022 year</Navbar.Brand>
+        </Nav>
+        <Nav className="justify-content-center">
+          <Image roundedCircle />
+        </Nav>
+        <Nav className="justify-content-end">
+          <Nav.Link eventKey="link-1">Author1</Nav.Link>
+          <Nav.Link eventKey="link-2">Author2</Nav.Link>
+          <Nav.Link eventKey="link-3">Author3</Nav.Link>
+        </Nav>
       </Container>
     </Navbar>
   );
 }
 
 export default Footer;
+
+/*
+      <Navbar bg="primary" variant="dark" expand="sm">
+        <Container fluid>
+          <Navbar.Toggle aria-controls="navbar-main" />
+          <Navbar.Brand href="/">RS Lang</Navbar.Brand>
+          <Navbar.Offcanvas id="navbar-main">
+            <Offcanvas.Header closeButton>
+              <Offcanvas.Title>RS Lang</Offcanvas.Title>
+            </Offcanvas.Header>
+            <Offcanvas.Body>
+              <Nav className="me-auto">
+                <Nav.Link href="/dictionary">Dictionary</Nav.Link>
+                <NavDropdown title="Mini-games" id="nav-dropdown-games">
+                  <NavDropdown.Item href="/games/audio-challenge">Audio Challenge</NavDropdown.Item>
+                  <NavDropdown.Item href="/games/sprint">Sprint</NavDropdown.Item>
+                </NavDropdown>
+                <Nav.Link href="/statistics">Statistics</Nav.Link>
+              </Nav>
+            </Offcanvas.Body>
+          </Navbar.Offcanvas>
+        </Container>
+      </Navbar>
+*/
