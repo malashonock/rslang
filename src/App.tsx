6c631416469c2388a21a459ecee65ef58ee84042
Stanlys <stanlys@yandex.ru>
2022-08-19 13:14:42 +0300

    init: start main-page footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
+import { Container } from 'react-bootstrap';
 import styles from './App.module.scss';
 
 export default function App(): JSX.Element {
-  return <div className={styles.appRoot}>RS Lang App</div>;
+  return (
+    <div className={styles.appRoot}>
+      <Container>
+        <h1>Main page content</h1>
+      </Container>
+    </div>
+  );
 }
