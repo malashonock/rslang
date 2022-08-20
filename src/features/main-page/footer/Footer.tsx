fb251e2b392caea606c3ff1c8e43944e9698488e
stanlys <stanlys@yandex.ru>
2022-08-20 09:08:58 +0300

    fix: fix some bugs to review

--- src/features/main-page/footer/Footer.tsx
+++ src/features/main-page/footer/Footer.tsx
@@ -1,29 +1,29 @@
 import { Container, Navbar, Image, Nav } from 'react-bootstrap';
 import github from '../../../assets/github.png';
-import rssschool from '../../../assets/rs_school_js.svg';
+import rsschool from '../../../assets/rs_school_js.svg';
 import styles from './Footer.module.scss';
 
 function Footer(): JSX.Element {
   return (
-    <Navbar fixed="bottom" bg="primary" variant="dark" expand="sm">
+    <Navbar bg="primary" variant="dark" expand="sm">
       <Container className="justify-content-between">
         <Nav>
           <Navbar.Brand href="#home">RSLang 2022 year</Navbar.Brand>
         </Nav>
         <Nav className="justify-content-center">
           <Nav.Link href="https://rollingscopes.com/">
-            <Image className={styles.logo_rss} src={rssschool} />
+            <Image className={styles.logoRss} src={rsschool} />
           </Nav.Link>
         </Nav>
-        <Nav className="justify-content-end d-flex">
-          <Image className={styles.logo} src={github} />
-          <Nav.Link eventKey="link-1" href="https://github.com/malashonock">
+        <Nav className="d-flex align-items-center">
+          <Image className={styles.logoGitHub} src={github} />
+          <Nav.Link eventKey="Author-1" href="https://github.com/malashonock">
             malashonock
           </Nav.Link>
-          <Nav.Link eventKey="link-2" href="https://github.com/qrvck">
+          <Nav.Link eventKey="Author-2" href="https://github.com/qrvck">
             qrvck
           </Nav.Link>
-          <Nav.Link eventKey="link-3" href="https://github.com/stanlys">
+          <Nav.Link eventKey="Author-3" href="https://github.com/stanlys">
             stanlys
           </Nav.Link>
         </Nav>
@@ -33,8 +33,3 @@ function Footer(): JSX.Element {
 }
 
 export default Footer;
-
-/*
-<Navbar bg="primary" variant="dark" expand="sm">
-      <Container fluid className="d-flex"> {styles.main_footer}
-*/
