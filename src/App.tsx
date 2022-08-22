9e36c9fb39ef9a6319af2346e0c9dbfbe87d19a0
Stanlys <stanlys@yandex.ru>
2022-08-22 21:15:34 +0300

    feat: login form with formik

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,14 @@ import { Container } from 'react-bootstrap';
 import { Outlet } from 'react-router-dom';
 import styles from './App.module.scss';
 import Footer from './features/main-page/footer/Footer';
-import Login from './features/auth/login-form/LoginForm';
+import LoginForm from './features/auth/login-form/LoginForm';
 import NavMenu from './features/main-page/nav-menu/NavMenu';
 
 const App = (): JSX.Element => {
   return (
     <div className={styles.appRoot}>
       <NavMenu />
-      <Login />
+      <LoginForm />
       <Container>
         <Outlet />
       </Container>
--- src/features/auth/login-form/LoginForm.tsx
+++ src/features/auth/login-form/LoginForm.tsx
@@ -7,149 +7,133 @@ import Button from '@mui/material/Button';
 import InputLabel from '@mui/material/InputLabel';
 import InputAdornment from '@mui/material/InputAdornment';
 import FormControl from '@mui/material/FormControl';
-import TextField from '@mui/material/TextField';
 import Visibility from '@mui/icons-material/Visibility';
 import VisibilityOff from '@mui/icons-material/VisibilityOff';
 import PersonIcon from '@mui/icons-material/Person';
 import { Email } from '@mui/icons-material';
+import * as yup from 'yup';
+import { useFormik } from 'formik';
+import { FormHelperText } from '@mui/material';
+
+const validationSchema = yup.object({
+  userName: yup.string(),
+  userEmail: yup.string().email('Enter a valid email').required('Email is required'),
+  userPassword: yup
+    .string()
+    .min(8, 'Password should be of minimum 8 characters length')
+    .required('Password is required'),
+});
 
 interface LoginFormState {
-  userName: string;
-  userEmail: string;
-  password: string;
-  showPassword: boolean;
   isNewUser: boolean;
-  isValidName: boolean;
-  isValidEmail: boolean;
-  isValidPassword: boolean;
-  isValidForm: boolean;
-  errorUserName: string;
-  errorUserEmail: string;
-  errorPassword: string;
+  isShowPassword: boolean;
 }
 
-export default function LoginForm() {
+const LoginForm = (): JSX.Element => {
   const [formState, setFormState] = React.useState<LoginFormState>({
-    userName: '',
-    userEmail: '',
-    password: '',
-    showPassword: false,
+    isShowPassword: false,
     isNewUser: false,
-    isValidName: true,
-    isValidEmail: true,
-    isValidPassword: true,
-    isValidForm: false,
-    errorUserName: '',
-    errorPassword: '',
-    errorUserEmail: '',
   });
 
-  const handleChange =
-    (prop: keyof LoginFormState) => (event: React.ChangeEvent<HTMLInputElement>) => {
-      setFormState({ ...formState, [prop]: event.target.value });
-    };
-
-  const checkPasswordMinLength = (password: string): boolean => password.length > 8;
-
-  const checkPassword = (): void => {
-    const { password } = formState;
-    setFormState({
-      ...formState,
-      isValidPassword: checkPasswordMinLength(password),
-    });
-  };
-
-  const checkEmail = (): void => {
-    const { userEmail } = formState;
-    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    setFormState({
-      ...formState,
-      isValidEmail: emailPattern.test(userEmail),
-    });
-  };
-
-  const checkUserName = (): void => {
-    const { userName } = formState;
-    if (!userName)
-      setFormState({
-        ...formState,
-        isValidName: false,
-      });
-  };
-
-  const onSubmite = () => {
-    checkPassword();
-    checkEmail();
-    if (formState.isNewUser) checkUserName();
-  };
-
-  const handleClickShowPassword = () => {
-    setFormState({
-      ...formState,
-      showPassword: !formState.showPassword,
-    });
-  };
-
-  const handleClickShowRegisterForm = () => {
-    setFormState({
-      ...formState,
-      isNewUser: !formState.isNewUser,
-    });
-  };
+  const formik = useFormik({
+    initialValues: {
+      userName: '',
+      userEmail: '',
+      userPassword: '',
+    },
+    validationSchema,
+    onSubmit: (values) => {
+      alert(JSON.stringify(values, null, 2));
+    },
+  });
 
   const handleMouseDownPassword = (event: React.MouseEvent<HTMLButtonElement>) => {
     event.preventDefault();
   };
 
+  const handleClickShowRegisterForm = (): void => {
+    setFormState({ ...formState, isNewUser: !formState.isNewUser });
+  };
+
+  const handleClickShowPassword = () => {
+    setFormState({
+      ...formState,
+      isShowPassword: !formState.isShowPassword,
+    });
+  };
+
   return (
-    <Box>
-      <div className="d-flex flex-column align-items-center justify-content-center">
+    <form onSubmit={formik.handleSubmit}>
+      <Box className="d-flex flex-column align-items-center justify-content-center">
         <h3>{formState.isNewUser ? 'REGISTER' : 'WELCOME'}</h3>
-        <TextField
-          label="E-mail"
-          id="user-email"
-          sx={{ m: 2, width: '40ch' }}
-          type="email"
-          error={!formState.isValidEmail}
-          fullWidth
-          autoFocus
-          required
-          InputProps={{
-            startAdornment: (
-              <InputAdornment position="start">
-                <Email />
-              </InputAdornment>
-            ),
-          }}
+        <FormControl
           variant="standard"
-          onChange={handleChange('userEmail')}
-        />
-        <TextField
-          label="User Name"
-          id="user-name"
           sx={{ m: 2, width: '40ch', display: formState.isNewUser ? 'block' : 'none' }}
-          type="text"
-          fullWidth
-          error={!formState.isValidName}
-          InputProps={{
-            startAdornment: (
+        >
+          <InputLabel htmlFor="userName">User name</InputLabel>
+          <Input
+            id="userName"
+            name="userName"
+            value={formik.values.userName}
+            required
+            fullWidth
+            onChange={formik.handleChange}
+            error={formik.touched.userName && Boolean(formik.errors.userName)}
+            endAdornment={
               <InputAdornment position="start">
                 <PersonIcon />
               </InputAdornment>
-            ),
-          }}
-          variant="standard"
-          onChange={handleChange('userName')}
-        />
+            }
+          />
+          <FormHelperText
+            id="standard-helper-for-name"
+            error
+            className="invalid-feedback"
+            sx={{
+              display:
+                formik.touched.userName && Boolean(formik.errors.userName) ? 'block' : 'none',
+            }}
+          >
+            {formik.touched.userName && formik.errors.userName}
+          </FormHelperText>
+        </FormControl>
+        <FormControl sx={{ m: 2, width: '40ch' }} variant="standard">
+          <InputLabel htmlFor="user-email">E-mail</InputLabel>
+          <Input
+            id="user-email"
+            name="userEmail"
+            value={formik.values.userEmail}
+            required
+            onChange={formik.handleChange}
+            error={formik.touched.userEmail && Boolean(formik.errors.userEmail)}
+            endAdornment={
+              <InputAdornment position="start">
+                <Email />
+              </InputAdornment>
+            }
+          />
+          <FormHelperText
+            id="standard-helper-for-email"
+            error
+            className="invalid-feedback"
+            sx={{
+              display:
+                formik.touched.userEmail && Boolean(formik.errors.userEmail) ? 'block' : 'none',
+            }}
+          >
+            {formik.touched.userEmail && formik.errors.userEmail}
+          </FormHelperText>
+        </FormControl>
         <FormControl sx={{ m: 2, width: '40ch' }} variant="standard">
           <InputLabel htmlFor="user-password">Password</InputLabel>
           <Input
             id="user-password"
-            type={formState.showPassword ? 'text' : 'password'}
-            value={formState.password}
+            type={formState.isShowPassword ? 'text' : 'password'}
+            name="userPassword"
+            value={formik.values.userPassword}
             required
-            onChange={handleChange('password')}
-            error={!formState.isValidName}
+            onChange={formik.handleChange}
+            error={formik.touched.userPassword && Boolean(formik.errors.userPassword)}
             endAdornment={
               <InputAdornment position="end">
                 <IconButton
@@ -157,32 +141,42 @@ export default function LoginForm() {
                   onClick={handleClickShowPassword}
                   onMouseDown={handleMouseDownPassword}
                 >
-                  {formState.showPassword ? <VisibilityOff /> : <Visibility />}
+                  {formState.isShowPassword ? <VisibilityOff /> : <Visibility />}
                 </IconButton>
               </InputAdornment>
             }
           />
+          <FormHelperText
+            id="standard-helper-for-password"
+            error
+            className="invalid-feedback"
+            sx={{
+              display:
+                formik.touched.userPassword && Boolean(formik.errors.userPassword)
+                  ? 'block'
+                  : 'none',
+            }}
+          >
+            {formik.touched.userPassword && formik.errors.userPassword}
+          </FormHelperText>
         </FormControl>
         <Stack direction="row" spacing={15} sx={{ display: 'flex', justifyContent: 'center' }}>
-          <Button
-            variant="contained"
-            color="success"
-            sx={{ m: 2, width: '40ch' }}
-            onSubmit={onSubmite}
-          >
-            {!formState.isNewUser ? 'LOGIN' : 'REGISTER'}
+          <Button type="submit" variant="contained" color="success" sx={{ m: 2, width: '40ch' }}>
+            {formState.isNewUser ? 'REGISTER' : 'LOG IN'}
           </Button>
         </Stack>
         <Stack direction="row" spacing={2}>
           <Button
             href="#text-buttons"
-            onClick={handleClickShowRegisterForm}
             sx={{ m: 2, width: '40ch' }}
+            onClick={handleClickShowRegisterForm}
           >
-            {!formState.isNewUser ? 'REGISTER' : 'LOG IN'}
+            {formState.isNewUser ? 'LOG IN' : 'REGISTER'}
           </Button>
         </Stack>
-      </div>
-    </Box>
+      </Box>
+    </form>
   );
-}
+};
+
+export default LoginForm;
