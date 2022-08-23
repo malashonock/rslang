8ce4f3d385e0bbe124cffe82f79220a3f721a909
qrvck <v.kapitan2010@gmail.com>
2022-08-23 20:49:25 +0300

    refactor: hide implementation details of getWords API

--- src/api/words.ts
+++ src/api/words.ts
@@ -3,9 +3,12 @@ import Word from '../interfaces/Word';
 
 const baseUrl = 'https://rslang-team159-be.herokuapp.com/words';
 
-const getWords = async (group: string, page: string): Promise<AxiosResponse<Word[], undefined>> => {
-  const response = await axios.get(`${baseUrl}?group=${group}&page=${page}`);
-  return response;
+const getWords = async (group: string, page: string): Promise<Word[]> => {
+  const response: AxiosResponse<Word[], undefined> = await axios.get(
+    `${baseUrl}?group=${group}&page=${page}`
+  );
+
+  return response.data;
 };
 
 export default getWords;
