e8643736d7b6fd8f03b80a3135059a1d457a9dca
Stanlys <stanlys@yandex.ru>
2022-08-24 16:33:05 +0300

    fix: remove loginform from app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { Container } from 'react-bootstrap';
 import { Outlet } from 'react-router-dom';
 import styles from './App.module.scss';
-import LoginForm from './features/auth/login-form/LoginForm';
 import Footer from './features/main-page/footer/Footer';
 import NavMenu from './features/main-page/nav-menu/NavMenu';
 
@@ -9,7 +8,6 @@ const App = (): JSX.Element => {
   return (
     <div className={styles.appRoot}>
       <NavMenu />
-      <LoginForm />
       <Container>
         <Outlet />
       </Container>
