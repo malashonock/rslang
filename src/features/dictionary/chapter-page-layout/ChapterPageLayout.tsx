b80da00edffcf934cc85767a7f75c5b434d17f00
qrvck <v.kapitan2010@gmail.com>
2022-08-29 05:00:05 +0300

    feat: do display of cards by chapters

--- src/features/dictionary/chapter-page-layout/ChapterPageLayout.tsx
+++ src/features/dictionary/chapter-page-layout/ChapterPageLayout.tsx
@@ -1,5 +1,26 @@
+import { useParams } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { getWords } from '../../../api/words';
+import WordCard from '../word-card/WordCard';
+
 const ChapterPageLayout = () => {
-  return <div>cards with words</div>;
+  const { chapter, page } = useParams();
+  const [wordCards, updateWordCards] = useState<JSX.Element[]>([]);
+
+  useEffect(() => {
+    const loadWords = async () => {
+      if (chapter && page) {
+        const words = await getWords(+chapter - 1, +page - 1);
+        const cards = words.map((word) => <WordCard word={word} isAuthorized={false} />);
+        updateWordCards(cards);
+      }
+    };
+
+    // eslint-disable-next-line @typescript-eslint/no-floating-promises
+    loadWords();
+  });
+
+  return <div>{wordCards}</div>;
 };
 
 export default ChapterPageLayout;
