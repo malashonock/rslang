4acd5e1e86b4d9bed244ea591a9545ce4f3df693
Stanlys <stanlys@yandex.ru>
2022-09-02 00:03:00 +0300

    init: install chartJS

--- src/features/statistics/Statistics.tsx
+++ src/features/statistics/Statistics.tsx
@@ -1,6 +1,51 @@
-import { Paper, Link, Alert, AlertTitle, Button } from '@mui/material';
+import { Paper, Alert, AlertTitle, Button } from '@mui/material';
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+import { Bar } from 'react-chartjs-2';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { faker } from '@faker-js/faker';
 import { useAppSelector } from '../../store/hooks';
 import style from './Statistics.module.scss';
+
+ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend);
+
+export const options = {
+  responsive: true,
+  plugins: {
+    legend: {
+      position: 'top' as const,
+    },
+    title: {
+      display: true,
+      text: 'User statistic',
+    },
+  },
+};
+
+const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+
+export const data = {
+  labels,
+  datasets: [
+    {
+      label: 'Dataset 1',
+      data: labels.map(() => faker.datatype.number({ min: 0, max: 1000 })),
+      backgroundColor: 'rgba(255, 99, 132, 0.5)',
+    },
+    {
+      label: 'Dataset 2',
+      data: labels.map(() => faker.datatype.number({ min: 0, max: 1000 })),
+      backgroundColor: 'rgba(53, 162, 235, 0.5)',
+    },
+  ],
+};
 
 const Statistics = (): JSX.Element => {
   const userAuth = useAppSelector((state) => state.authorization);
@@ -10,6 +55,7 @@ const Statistics = (): JSX.Element => {
       {userAuth.authorizeStatus && (
         <Paper elevation={3} className={style.infoArea}>
           Goal
+          <Bar options={options} data={data} />
         </Paper>
       )}
       {!userAuth.authorizeStatus && (
