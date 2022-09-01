5499778e49084bd0b1d02bf2a9adbd779ca597e2
Stanlys <stanlys@yandex.ru>
2022-09-01 23:07:35 +0300

    feat: hide routers for nonauth user

--- src/features/statistics/Statistics.tsx
+++ src/features/statistics/Statistics.tsx
@@ -1,5 +1,30 @@
+import { Paper, Link, Alert, AlertTitle, Button } from '@mui/material';
+import { useAppSelector } from '../../store/hooks';
+import style from './Statistics.module.scss';
+
 const Statistics = (): JSX.Element => {
-  return <h2>Statistics</h2>;
+  const userAuth = useAppSelector((state) => state.authorization);
+  return (
+    <>
+      <h2>{userAuth.name} statistics</h2>
+      {userAuth.authorizeStatus && (
+        <Paper elevation={3} className={style.infoArea}>
+          Goal
+        </Paper>
+      )}
+      {!userAuth.authorizeStatus && (
+        <Paper elevation={3} className={style.infoArea}>
+          <Alert severity="warning" className={style.loginForm}>
+            <AlertTitle>Dear Guest.To view statistics, please register.</AlertTitle>
+            <Button variant="outlined" color="info" href="/auth">
+              {' '}
+              I WANT SIGN UP
+            </Button>
+          </Alert>
+        </Paper>
+      )}
+    </>
+  );
 };
 
 export default Statistics;
