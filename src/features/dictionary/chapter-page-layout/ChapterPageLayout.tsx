cc21ffc804a95c5eea3ad24337550a2e64dcf56e
qrvck <v.kapitan2010@gmail.com>
2022-09-05 01:11:56 +0300

    feat: add check if the word was used by the user

--- src/features/dictionary/chapter-page-layout/ChapterPageLayout.tsx
+++ src/features/dictionary/chapter-page-layout/ChapterPageLayout.tsx
@@ -3,23 +3,47 @@ import { useEffect, useState } from 'react';
 import { getWords } from '../../../api/words';
 import WordCard from '../word-card/WordCard';
 import Word from '../../../model/Word';
+import { UserWord } from '../../../model/UserWord';
 import styles from './ChapterPageLayout.module.scss';
+import { useAppSelector } from '../../../store/hooks';
+import { getUserWords } from '../../../api/userWords';
 
 const ChapterPageLayout = () => {
+  const { authorizeStatus, id } = useAppSelector((state) => state.authorization);
   const { chapter, page } = useParams();
-  const [displayedWords, updateDisplayedWords] = useState<Word[]>([]);
+  const [displayedWords, updateDisplayedWords] = useState<Array<Word & UserWord> | Array<Word>>([]);
 
   useEffect(() => {
     const loadWords = async () => {
       if (chapter && page) {
         const newWords = await getWords(+chapter - 1, +page - 1);
-        updateDisplayedWords(newWords);
+
+        if (authorizeStatus) {
+          const userWords = await getUserWords(id);
+          const updatingNewWords = newWords.map((word) => {
+            const isActiveWord = userWords.find((userWord) => userWord.wordId === word.id);
+            if (isActiveWord) return { ...word, ...userWords };
+
+            const inactiveWordOptions = {
+              wasPlayed: false,
+              correctGuessCount: 0,
+              wrongGuessCount: 0,
+              isDifficult: false,
+              isLearned: false,
+            };
+
+            return { ...word, ...inactiveWordOptions };
+          });
+          updateDisplayedWords(updatingNewWords);
+        } else {
+          updateDisplayedWords(newWords);
+        }
       }
     };
 
     // eslint-disable-next-line @typescript-eslint/no-floating-promises
     loadWords();
-  }, [chapter, page]);
+  }, [authorizeStatus, chapter, id, page]);
 
   return (
     <div className={styles.cards}>
