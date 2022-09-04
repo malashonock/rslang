9353c0460cbc2da4173d14e313ab907bcc743b6c
qrvck <v.kapitan2010@gmail.com>
2022-09-04 14:55:04 +0300

    feat: do create user word API

--- src/api/userWords.ts
+++ src/api/userWords.ts
@@ -1,12 +1,23 @@
 import { AxiosResponse } from 'axios';
 import instanceAxios from './httpConfig';
-import UserWord from '../model/UserWord';
+import { UserWord, NewUserWord } from '../model/UserWord';
 
-const getUserWords = async (userId: string): Promise<UserWord[]> => {
+export const getUserWords = async (userId: string): Promise<UserWord[]> => {
   const response: AxiosResponse<UserWord[], undefined> = await instanceAxios.get(
     `/users/${userId}/words`
   );
   return response.data;
 };
 
-export default getUserWords;
+export const createUserWord = async (
+  userId: string,
+  wordId: string,
+  newWord: NewUserWord
+): Promise<UserWord> => {
+  const response: AxiosResponse<UserWord, NewUserWord> = await instanceAxios.post(
+    `/users/${userId}/words/${wordId}`,
+    newWord
+  );
+
+  return response.data;
+};
