fee1e3e743052a9ecccf7173ba68fc43eae7b512
qrvck <v.kapitan2010@gmail.com>
2022-09-04 18:41:31 +0300

    feat :add updateUserWord API

--- src/api/userWords.ts
+++ src/api/userWords.ts
@@ -1,12 +1,23 @@
 import { AxiosResponse } from 'axios';
 import instanceAxios from './httpConfig';
-import UserWord from '../model/UserWord';
+import { UserWord, UpdatingUserWord } from '../model/UserWord';
 
-const getUserWords = async (userId: string): Promise<UserWord[]> => {
+export const getUserWords = async (userId: string): Promise<UserWord[]> => {
   const response: AxiosResponse<UserWord[], undefined> = await instanceAxios.get(
     `/users/${userId}/words`
   );
   return response.data;
 };
 
-export default getUserWords;
+export const updateUserWord = async (
+  userId: string,
+  wordId: string,
+  updatingWord: UpdatingUserWord
+) => {
+  const response: AxiosResponse<UserWord, undefined> = await instanceAxios.put(
+    `/users/${userId}/words/${wordId}`,
+    updatingWord
+  );
+
+  return response.data;
+};
