5d64a04c11a06689959914150a97c4d721ee5b01
Stanlys <stanlys@yandex.ru>
2022-09-05 16:38:44 +0300

    feat: hide footer for game pages

--- src/features/main-page/footer/Footer.tsx
+++ src/features/main-page/footer/Footer.tsx
@@ -1,31 +1,40 @@
 import { Container, Navbar, Image, Nav } from 'react-bootstrap';
+import { useLocation } from 'react-router-dom';
 import github from '../../../assets/github.png';
 import rsschool from '../../../assets/rs_school_js.svg';
 import styles from './Footer.module.scss';
 
 const Footer = (): JSX.Element => {
+  const local = useLocation();
+
+  const checkPage = (): boolean => {
+    return !local.pathname.includes('games');
+  };
+
   return (
     <Navbar bg="primary" variant="dark" expand="sm">
-      <Container className="justify-content-between">
-        <Nav className="d-flex gap-2">
-          <Nav.Link href="https://rollingscopes.com/">
-            <Image className={styles.logoRss} src={rsschool} />
-          </Nav.Link>
-          <Nav.Item className="text-light d-flex align-items-center">2022</Nav.Item>
-        </Nav>
-        <Nav className="d-flex align-items-center">
-          <Image className={styles.logoGitHub} src={github} />
-          <Nav.Link eventKey="Author-1" href="https://github.com/malashonock">
-            malashonock
-          </Nav.Link>
-          <Nav.Link eventKey="Author-2" href="https://github.com/qrvck">
-            qrvck
-          </Nav.Link>
-          <Nav.Link eventKey="Author-3" href="https://github.com/stanlys">
-            stanlys
-          </Nav.Link>
-        </Nav>
-      </Container>
+      {checkPage() && (
+        <Container className="justify-content-between">
+          <Nav className="d-flex gap-2">
+            <Nav.Link href="https://rollingscopes.com/">
+              <Image className={styles.logoRss} src={rsschool} />
+            </Nav.Link>
+            <Nav.Item className="text-light d-flex align-items-center">2022</Nav.Item>
+          </Nav>
+          <Nav className="d-flex align-items-center">
+            <Image className={styles.logoGitHub} src={github} />
+            <Nav.Link eventKey="Author-1" href="https://github.com/malashonock">
+              malashonock
+            </Nav.Link>
+            <Nav.Link eventKey="Author-2" href="https://github.com/qrvck">
+              qrvck
+            </Nav.Link>
+            <Nav.Link eventKey="Author-3" href="https://github.com/stanlys">
+              stanlys
+            </Nav.Link>
+          </Nav>
+        </Container>
+      )}
     </Navbar>
   );
 };
