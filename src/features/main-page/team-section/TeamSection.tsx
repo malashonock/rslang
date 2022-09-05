ea23a9fa414081daa7f95bf8092a773e3f12ac09
Yan Malashonak <malashonock@gmail.com>
2022-09-05 19:52:50 +0300

    refactor: prettify team memebers contribution

--- src/features/main-page/team-section/TeamSection.tsx
+++ src/features/main-page/team-section/TeamSection.tsx
@@ -4,24 +4,23 @@ import styles from './TeamSection.module.scss';
 import github from '../../../assets/github.png';
 
 const TeamSection = (): JSX.Element => {
-  const texttoList = (features: string): JSX.Element => {
-    const featireList = features
-      .split(', ')
-      .map((feature) => <ListGroup.Item>{feature}</ListGroup.Item>);
-    return <ListGroup variant="flush">{featireList}</ListGroup>;
-  };
-
   return (
     <section>
       <h2 className={styles.teamTitle}>RSSBand</h2>
       <section className={styles.team}>
-        {teamMembers.map(({ id, ghLink, imgSrc, name, role, work }) => (
+        {teamMembers.map(({ id, ghLink, imgSrc, name, role, contribution }) => (
           <Card className={styles.card} key={id}>
             <Card.Img src={imgSrc} alt={`${role}-${name}`} />
             <Card.Body className={styles.cardElements}>
               <Card.Title>{name}</Card.Title>
-              <Card.Text>{role}</Card.Text>
-              <Card.Text>{texttoList(work)}</Card.Text>
+              <Card.Text className="fw-semibold">{role}</Card.Text>
+              <Card.Text>
+                <ListGroup variant="flush">
+                  {contribution.map((feature: string) => (
+                    <ListGroup.Item className="text-center">{feature}</ListGroup.Item>
+                  ))}
+                </ListGroup>
+              </Card.Text>
             </Card.Body>
             <Card.Body className={styles.cardFooter}>
               <a href={ghLink} title="Посмотреть Github аккаунт">
