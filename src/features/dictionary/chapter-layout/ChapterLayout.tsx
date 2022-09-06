46b34cbc9f048c9cc09408e534c7f88895af0af5
Yan Malashonak <malashonock@gmail.com>
2022-09-06 18:15:47 +0300

    feat: add links to mini-games to dictionary

--- src/features/dictionary/chapter-layout/ChapterLayout.tsx
+++ src/features/dictionary/chapter-layout/ChapterLayout.tsx
@@ -1,11 +1,16 @@
 import { Outlet } from 'react-router-dom';
+import { Stack } from 'react-bootstrap';
 import styles from './ChapterLayout.module.scss';
 import ChapterPagesSelector from '../chapter-pages-selector/ChapterPagesSelector';
+import GamesLinks from '../games-links/GamesLinks';
 
 const ChapterLayout = () => {
   return (
     <>
-      <ChapterPagesSelector className={styles.switchChapterPages} />
+      <Stack className={styles.switchChapterPages} gap={2}>
+        <ChapterPagesSelector />
+        <GamesLinks />
+      </Stack>
       <div className={styles.wordCardsWrapper}>
         <Outlet />
       </div>
