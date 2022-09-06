d9ffa700f607c7dd07b1bc4d2c662f617d5ddd20
qrvck <v.kapitan2010@gmail.com>
2022-09-06 07:00:09 +0300

    feat: add ability to array sound sources to sound button and ability to press stop during playback

--- src/features/shared/sound-button/SoundButton.tsx
+++ src/features/shared/sound-button/SoundButton.tsx
@@ -1,11 +1,12 @@
-import { CSSProperties, useState } from 'react';
+import { CSSProperties, useRef, useState } from 'react';
 import { Button } from 'react-bootstrap';
 import playAudioAsync from '../../../utils/sound';
 import styles from './SoundButton.module.scss';
-import { ReactComponent as SoundIcon } from '../../../assets/icons/sound.svg';
+import { ReactComponent as PlaySoundIcon } from '../../../assets/icons/play-sound.svg';
+import { ReactComponent as StopSoundIcon } from '../../../assets/icons/stop-sound.svg';
 
 interface SoundButtonProps {
-  soundSrc: string;
+  soundSrc: string | string[];
   diameter?: string;
   variant?: 'primary' | 'secondary' | 'warning' | 'danger' | 'info';
 }
@@ -15,23 +16,44 @@ const SoundButton = ({
   diameter = '2rem',
   variant = 'primary',
 }: SoundButtonProps): JSX.Element => {
-  const [disabled, setDisabled] = useState(false);
+  const [played, setPlayedState] = useState(false);
+  const sound = useRef<HTMLAudioElement>();
 
   const play = async () => {
-    setDisabled(true);
-    const sound = new Audio(soundSrc);
-    await playAudioAsync(sound);
-    setDisabled(false);
+    if (!played) {
+      setPlayedState(true);
+      if (typeof soundSrc === 'string') {
+        sound.current = new Audio(soundSrc);
+        await playAudioAsync(sound.current);
+      } else {
+        // eslint-disable-next-line no-restricted-syntax
+        for (const i of soundSrc) {
+          sound.current = new Audio(i);
+          // eslint-disable-next-line no-await-in-loop
+          await playAudioAsync(sound.current);
+        }
+      }
+      setPlayedState(false);
+    } else {
+      setPlayedState(false);
+      if (sound.current) {
+        sound.current.currentTime = 0;
+        sound.current.pause();
+      }
+    }
   };
 
   return (
     <Button
       style={{ '--diameter': diameter } as CSSProperties}
       onClick={play}
-      disabled={disabled}
       className={`${styles.soundButton} btn btn-${variant} rounded-circle`}
     >
-      <SoundIcon className={styles.soundIcon} />
+      {played ? (
+        <StopSoundIcon className={styles.soundIcon} />
+      ) : (
+        <PlaySoundIcon className={styles.soundIcon} />
+      )}
     </Button>
   );
 };
