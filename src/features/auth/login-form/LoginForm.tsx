d33a4648f89a6fa28068cb1291517e313274fc70
Stanlys <stanlys@yandex.ru>
2022-09-06 12:27:06 +0300

    merge develop

--- src/features/auth/login-form/LoginForm.tsx
+++ src/features/auth/login-form/LoginForm.tsx
@@ -58,7 +58,7 @@ const LoginForm = (): JSX.Element => {
           refreshToken: '',
         };
         dispatch(createUserData(userData));
-        // // redirectToMainPage();
+        navigate(-1);
       } catch {
         setIsRegisterSuccess(false);
         setIsServerError(true);
@@ -77,10 +77,10 @@ const LoginForm = (): JSX.Element => {
           refreshToken: existUser.refreshToken,
         };
         dispatch(setAuthorizeUser(userData));
+        navigate(-1);
       } catch {
         setIsServerError(true);
       }
-      // // if (!isServerError) setTimeout(redirectToMainPage, 5000);
     }
   };
 
@@ -105,7 +105,7 @@ const LoginForm = (): JSX.Element => {
           <>
             <Avatar className={styles.error}>{isServerError && <ErrorIcon />}</Avatar>
             <FormHelperText error>
-              <span>Error in Server response</span>
+              <span>Incorrect email or password, please try again</span>
             </FormHelperText>
           </>
         )}
--- src/features/dictionary/chapter-layout/ChapterLayout.tsx
+++ src/features/dictionary/chapter-layout/ChapterLayout.tsx
@@ -1,11 +1,16 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useParams } from 'react-router-dom';
 import styles from './ChapterLayout.module.scss';
 import ChapterPagesSelector from '../chapter-pages-selector/ChapterPagesSelector';
 
 const ChapterLayout = () => {
+  const { chapter } = useParams();
+
   return (
     <>
-      <ChapterPagesSelector className={styles.switchChapterPages} />
+      <ChapterPagesSelector
+        className={styles.switchChapterPages}
+        pageCount={chapter === '7' ? 1 : 30}
+      />
       <div className={styles.wordCardsWrapper}>
         <Outlet />
       </div>
